946d39a0964144f650c6afcfee90df1c56c0479c
attrivivek <vattri2@illinois.edu>
2018-12-05 21:06:13 -0600

    Vivek's new files - test1

---
+++ scripts/aggregate_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import math
+
+
+# Variables
+HOTEL_WITH_SCORING = "../data/hotels_4_with_review_score.csv"
+
+AGG_HOTELS = "../data/aggregated_hotels.csv"
+
+WEIGHTS = [20, 10, 5, 2, 0.00001]
+
+# Read Detailed hotels CSV
+
+data = pd.read_csv(HOTEL_WITH_SCORING)
+
+# Aggregate data
+
+aggregations = {'Neg_Review_Score': 'mean',
+                'Pos_Review_Score': 'mean',
+                'Reviewer.Score': 'mean',
+                'Average.Score': 'mean',
+                'Total.Reviews': 'mean',
+                }
+
+hotel_agg = data.groupby(['Hotel.Name']).agg(aggregations)
+
+# Aggregate all tags
+
+hotel_agg['Tags'] = data.groupby(['Hotel.Name'])['Tags'].apply(list)
+
+# Create an overall score
+
+hotel_agg['Overall.Score'] = WEIGHTS[0] * hotel_agg['Neg_Review_Score'] + \
+                             WEIGHTS[1] * hotel_agg['Pos_Review_Score'] + \
+                             WEIGHTS[2] * hotel_agg['Reviewer.Score'] +  \
+                             WEIGHTS[3] * hotel_agg['Average.Score'] + \
+                             WEIGHTS[4] * hotel_agg['Total.Reviews']
+
+
+# Write to aggregated csv
+
+hotel_agg.to_csv(AGG_HOTELS)
+
+
+
+
+
---
+++ scripts/search_indexing.py
@@ -0,0 +1,31 @@
+import metapy
+
+import pandas as pd
+
+AGG_HOTELS = "../data/aggregated_hotels.csv"
+
+
+hotel = pd.read_csv(AGG_HOTELS)
+
+doc = metapy.index.Document()
+# ana = metapy.analyzers.load('config.toml')
+
+idx = metapy.index.make_inverted_index('config.toml')
+
+
+query = metapy.index.Document()
+query.content("1 room")
+
+ranker = metapy.index.OkapiBM25()
+
+
+
+for result in ranker.score(idx, query, 10):
+    print hotel[hotel['Hotel.Name'] == idx.metadata(result[0]).get('name')]
+    #print idx.metadata(result[0]).get('name')
+
+
+#for tag in hotel['Tags']:
+# doc.content(hotel['Tags'][1])
+# print hotel['Tags'][1]
+# print(ana.analyze(doc))
