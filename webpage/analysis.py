7be232f828e217c8bcab5054a7f38096e65bb028
Scott <programmingbishop@gmail.com>
2018-12-05 20:27:54 -0600

    Added pseudocode

--- webpage/analysis.py
+++ webpage/analysis.py
@@ -6,9 +6,16 @@ import pandas as pd
 #vars
 HOTEL_WITH_SCORING = "../data/hotels_4_with_review_score.csv"
 
+def get_analysis(hotel_data, query):
+    
+
+    return 0
+
 def get_results(query):
     
-    d_hotel    = pd.read_csv(HOTEL_WITH_SCORING)
+    d_hotel = pd.read_csv(HOTEL_WITH_SCORING)
+    ret_values = get_analysis(d_hotel, query)
+
     ret_values = d_hotel.iloc[:,2:].head(20)  
     ret_values = ret_values.sort_values( by = ['Average.Score'], ascending = False )
 
