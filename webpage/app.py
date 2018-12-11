2b8a1d45855043c0c3caf1aa4c4a7c5d2e7a5414
Scott Bishop <scottbishopdesign@gmail.com>
2018-12-10 20:52:54 -0600

    Added error handling

--- webpage/app.py
+++ webpage/app.py
@@ -8,13 +8,6 @@ app = Flask(__name__)
 
 COUNTRIES = ["netherlands", "austria", "france", "italy", "kingdom", "spain"]
 
-
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     text = "Works!"
-#     return render_template('index.html', output = text)
-
 # This is the initial application the local server runs
 @app.route('/')
 def index():
@@ -25,47 +18,40 @@ def index():
 def result():
     if request.method == 'POST':
         pd.options.display.max_colwidth = 1000
-        # This variable is not used but kept for archival purposes for now
-        #data = pd.read_csv('../data/hotels_3.csv')
 
-        # Update this to replicate a query being submitted by the user
         query = request.form['hotel_query']
 
-        # query  = query
-        # if COUNTRIES in query 
-        #   get the country
-        #   output = analysis.get_results(query, country)
-        # elif COUNTRIES not in query
-        #   return message hotels in country not found; list countries
+        html = ''
 
-        output = analysis.get_results(query)
+        if query.lower() in COUNTRIES:
+            output = analysis.get_results(query)
 
-        html = ""
+            for index, row in output.iterrows():
+                html = (html  + "<a href='https://www.google.com/search?q=" + row['Hotel.Name'].strip() + "' target='_blank'>" + 
+                                    "<div class='hotel-row'>" +
+                                        "<div class='row'>" +
+                                            "<div class='col-sm-6 hotel-name'>" +
+                                                "<p class='name-label'>Hotel Name</p>" +
+                                                "<p>" + row['Hotel.Name'] + "</p>" +
+                                            "</div>" +
 
-        for index, row in output.iterrows():
-            html = (html  + "<a href='https://www.google.com/search?q=" + row['Hotel.Name'].strip() + "' target='_blank'>" + 
-                                "<div class='hotel-row'>" +
-                                    "<div class='row'>" +
-                                        "<div class='col-sm-6 hotel-name'>" +
-                                            "<p class='name-label'>Hotel Name</p>" +
-                                            "<p>" + row['Hotel.Name'] + "</p>" +
-                                        "</div>" +
+                                            "<div class='col-sm-3 hotel-score'>" +
+                                                "<p class='score-label'>Average Rating</p>" +
+                                                "<p>" + str( row['Average.Score'] ) + "</p>" +
+                                            "</div>" +
 
-                                        "<div class='col-sm-3 hotel-score'>" +
-                                            "<p class='score-label'>Average Rating</p>" +
-                                            "<p>" + str( row['Average.Score'] ) + "</p>" +
-                                        "</div>" +
+                                            "<div class='col-sm-3 hotel-review-count'>" +
+                                                "<p class='count-label'>Number of Reviews</p>" +
+                                                "<p>" + str( row['Total.Reviews'] ) + "</p>" +
+                                            "</div>" +
 
-                                        "<div class='col-sm-3 hotel-review-count'>" +
-                                            "<p class='count-label'>Number of Reviews</p>" +
-                                            "<p>" + str( row['Total.Reviews'] ) + "</p>" +
-                                        "</div>" +
+                                            "<div class='col-sm-12 hotel-address'>" +
+                                                "<p><span class='address-label'>Address: </span>" + row['Hotel.Address'] + "</p>" +
+                                            "</div>" +
+                                        "</div>"
+                                    "</div><hr>" +
+                                "</a>")
+        else:
+            html = '<p style="text-align: center;">Please use one of the listed country names in your query: Netherlands, Austria, France, Italy, Kingdom (United Kingdom), or Spain</p>'
 
-                                        "<div class='col-sm-12 hotel-address'>" +
-                                            "<p><span class='address-label'>Address: </span>" + row['Hotel.Address'] + "</p>" +
-                                        "</div>" +
-                                    "</div>"
-                                "</div><hr>" +
-                            "</a>")
-
-        return render_template( 'result.html', output = output )
+        return render_template( 'result.html', output = html )
