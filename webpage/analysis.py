7aab5f33c3cd985eec9b7bfc11e82a058c408f95
Scott Bishop <scottbishopdesign@gmail.com>
2018-12-12 20:32:05 -0600

    add country tokenizer

--- webpage/analysis.py
+++ webpage/analysis.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
-HOTEL_WITH_SCORING = "../data/hotels_4_with_review_score.csv"
+HOTEL_WITH_SCORING = "../data/hotels_5.csv"
+COUNTRIES          = ["netherlands", "austria", "france", "italy", "kingdom", "spain"]
+
+def get_tokenizers(query):
+    country_tokens = []
+
+    for word in query.split():
+        if word in COUNTRIES:
+            country_tokens.append(word)
+
+    return country_tokens
 
 def get_results(query):
     d_hotel = pd.read_csv(HOTEL_WITH_SCORING)
 
-    ret_values = d_hotel.iloc[:,2:].head(20)
+    country_tokens = get_tokenizers(query)
+
+    ret_values = d_hotel.iloc[:,1:].head(20)
     ret_values = ret_values.sort_values( by = ['Average.Score'], ascending = False )
 
     return ret_values
--- webpage/app.py
+++ webpage/app.py
@@ -25,31 +25,32 @@ def result():
         if any( s in query.lower() for s in COUNTRIES ):
             output = analysis.get_results(query)
 
-            for index, row in output.iterrows():
-                html = (html  + "<a href='https://www.google.com/search?q=" + row['Hotel.Name'].strip() + "' target='_blank'>" + 
-                                    "<div class='hotel-row'>" +
-                                        "<div class='row'>" +
-                                            "<div class='col-sm-6 hotel-name'>" +
-                                                "<p class='name-label'>Hotel Name</p>" +
-                                                "<p>" + row['Hotel.Name'] + "</p>" +
-                                            "</div>" +
+            # for index, row in output.iterrows():
+            #     html = (html  + "<a href='https://www.google.com/search?q=" + row['Hotel.Name'].strip() + "' target='_blank'>" + 
+            #                         "<div class='hotel-row'>" +
+            #                             "<div class='row'>" +
+            #                                 "<div class='col-sm-6 hotel-name'>" +
+            #                                     "<p class='name-label'>Hotel Name</p>" +
+            #                                     "<p>" + row['Hotel.Name'] + "</p>" +
+            #                                 "</div>" +
 
-                                            "<div class='col-sm-3 hotel-score'>" +
-                                                "<p class='score-label'>Average Rating</p>" +
-                                                "<p>" + str( row['Average.Score'] ) + "</p>" +
-                                            "</div>" +
+            #                                 "<div class='col-sm-3 hotel-score'>" +
+            #                                     "<p class='score-label'>Average Rating</p>" +
+            #                                     "<p>" + str( row['Average.Score'] ) + "</p>" +
+            #                                 "</div>" +
 
-                                            "<div class='col-sm-3 hotel-review-count'>" +
-                                                "<p class='count-label'>Number of Reviews</p>" +
-                                                "<p>" + str( row['Total.Reviews'] ) + "</p>" +
-                                            "</div>" +
+            #                                 "<div class='col-sm-3 hotel-review-count'>" +
+            #                                     "<p class='count-label'>Number of Reviews</p>" +
+            #                                     "<p>" + str( row['Total.Reviews'] ) + "</p>" +
+            #                                 "</div>" +
 
-                                            "<div class='col-sm-12 hotel-address'>" +
-                                                "<p><span class='address-label'>Address: </span>" + row['Hotel.Address'] + "</p>" +
-                                            "</div>" +
-                                        "</div>"
-                                    "</div><hr>" +
-                                "</a>")
+            #                                 "<div class='col-sm-12 hotel-address'>" +
+            #                                     "<p><span class='address-label'>Address: </span>" + row['Hotel.Address'] + "</p>" +
+            #                                 "</div>" +
+            #                             "</div>"
+            #                         "</div><hr>" +
+            #                     "</a>")
+            html = output
         else:
             html = '<p style="text-align: center;">Please use one of the listed country names in your query: Netherlands, Austria, France, Italy, Kingdom (United Kingdom), or Spain</p>'
 
