552e6957dfa14b562d8840d1fd5d43cec3205672
Scott <programmingbishop@gmail.com>
2018-12-13 19:52:33 -0600

    Clean up comments

--- webpage/analysis.py
+++ webpage/analysis.py
@@ -8,8 +8,6 @@ COUNTRIES          = ["Netherlands", "Austria", "France", "Italy", "Kingdom", "S
 d_hotel = pd.read_csv(HOTEL_WITH_SCORING)
 
 def get_tokenizers(query):
-    # Create temp pandas dataframe
-
     country_tokens = []
 
     for word in query.replace(',',' ').split():
@@ -18,17 +16,7 @@ def get_tokenizers(query):
 
     return country_tokens
 
-    # Instead of returning tokens return rows that have country name found. Use tokens to select rows
-
 def check_tags(query, country_dataframe):
-    # Create temp pandas dataframe
-
-    # for every hotel row
-        # check if query contains tags for that hotel
-            # If so add hotel row to pandas dataframe
-
-    # tags_dataframe = country_dataframe[any(tag in query for tag in country_dataframe['Tags'])]
-    # tags_dataframe = country_dataframe
 
     f = lambda x: re.split(',|[|]| ', x)
 
@@ -39,15 +27,8 @@ def check_tags(query, country_dataframe):
     return tags_dataframe
 
 def get_results(query):
-
-
-    # If update get_tokenizers update this variable
     country_dataframe = d_hotel[(d_hotel['Country']).isin(get_tokenizers(query))]
     tags_dataframe    = check_tags(query, country_dataframe)
-
-    # At this point you will have pandas dataframe with rows that match by country and tag
-    # Use this pandas dataframe to select rows with best score and return
-    # Make sure the pandas dataframe at this point is similar to the dataframe from HOTEL_WITH_SCORING
 
     ret_values = tags_dataframe.sort_values( by = ['Overall.Score'], ascending = False )
     ret_values = ret_values.head(20)
