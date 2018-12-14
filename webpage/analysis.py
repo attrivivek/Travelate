a2ecb2486b2ef4d34b9bc48ceb33ca0a03594139
Scott <programmingbishop@gmail.com>
2018-12-13 20:25:31 -0600

    update to check_tags function

--- webpage/analysis.py
+++ webpage/analysis.py
@@ -20,18 +20,26 @@ def check_tags(query, country_dataframe):
 
     f = lambda x: re.split(',|[|]| ', x)
 
-    for index, tags in enumerate(country_dataframe['Tags'].apply(f)):
-        if any(tag in query for tag in tags):
-            tags_dataframe = country_dataframe.loc[:index + 1]
+    # for index, tags in enumerate(country_dataframe['Tags'].apply(f)):
+    #     if any(tag in query for tag in tags):
+    #         tags_dataframe = country_dataframe.loc[:index + 1]
 
-    return tags_dataframe
+    # return tags_dataframe
+
+    query_indexes = []
+
+    for index, row in country_dataframe.iterrows():
+        if any( tag in query for tag in row.Tags ):
+            query_indexes.append( index )
+
+    return country_dataframe.shape
 
 def get_results(query):
     country_dataframe = d_hotel[(d_hotel['Country']).isin(get_tokenizers(query))]
     tags_dataframe    = check_tags(query, country_dataframe)
 
-    ret_values = tags_dataframe.sort_values( by = ['Overall.Score'], ascending = False )
-    ret_values = ret_values.head(20)
-    ret_values = tags_dataframe.sort_values( by = ['Average.Score'], ascending = False )
+    # ret_values = tags_dataframe.sort_values( by = ['Overall.Score'], ascending = False )
+    # # ret_values = ret_values.head(20)
+    # ret_values = tags_dataframe.sort_values( by = ['Average.Score'], ascending = False )
 
-    return ret_values
+    return tags_dataframe
--- webpage/app.py
+++ webpage/app.py
@@ -24,33 +24,33 @@ def result():
 
         if any( s in query.lower() for s in COUNTRIES ):
             output = analysis.get_results(query)
+            html = output
+            # for index, row in output.iterrows():
+            #     html = (html  + "<a href='https://www.google.com/search?q=" + row['Hotel.Name'].strip() + "' target='_blank'>" + 
+            #                         "<div class='hotel-row'>" +
+            #                             "<div class='row'>" +
+            #                                 "<div class='col-sm-6 hotel-name'>" +
+            #                                     "<p class='name-label'>Hotel Name</p>" +
+            #                                     "<p>" + row['Hotel.Name'] + "</p>" +
+            #                                 "</div>" +
 
-            for index, row in output.iterrows():
-                html = (html  + "<a href='https://www.google.com/search?q=" + row['Hotel.Name'].strip() + "' target='_blank'>" + 
-                                    "<div class='hotel-row'>" +
-                                        "<div class='row'>" +
-                                            "<div class='col-sm-6 hotel-name'>" +
-                                                "<p class='name-label'>Hotel Name</p>" +
-                                                "<p>" + row['Hotel.Name'] + "</p>" +
-                                            "</div>" +
+            #                                 "<div class='col-sm-3 hotel-score'>" +
+            #                                     "<p class='score-label'>Average Rating</p>" +
+            #                                     "<p>" + str( round(row['Average.Score'], 2) ) + "</p>" +
+            #                                 "</div>" +
 
-                                            "<div class='col-sm-3 hotel-score'>" +
-                                                "<p class='score-label'>Average Rating</p>" +
-                                                "<p>" + str( round(row['Average.Score'], 2) ) + "</p>" +
-                                            "</div>" +
+            #                                 "<div class='col-sm-3 hotel-review-count'>" +
+            #                                     "<p class='count-label'>Number of Reviews</p>" +
+            #                                     "<p>" + str( row['Total.Reviews'] ) + "</p>" +
+            #                                 "</div>" +
 
-                                            "<div class='col-sm-3 hotel-review-count'>" +
-                                                "<p class='count-label'>Number of Reviews</p>" +
-                                                "<p>" + str( row['Total.Reviews'] ) + "</p>" +
-                                            "</div>" +
+            #                                 "<div class='col-sm-12 hotel-address'>" +
+            #                                     "<p><span class='address-label'>Address: </span>" + row['Hotel.Address'] + "</p>" +
+            #                                 "</div>" +
 
-                                            "<div class='col-sm-12 hotel-address'>" +
-                                                "<p><span class='address-label'>Address: </span>" + row['Hotel.Address'] + "</p>" +
-                                            "</div>" +
-
-                                        "</div>"
-                                    "</div><hr>" +
-                                "</a>")
+            #                             "</div>"
+            #                         "</div><hr>" +
+            #                     "</a>")
         else:
             html = '<p style="text-align: center;">Please use one of the listed country names in your query: Netherlands, Austria, France, Italy, Kingdom (United Kingdom), or Spain</p>'
 
